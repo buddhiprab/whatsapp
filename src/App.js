069ba85b9aa6de462c2974d7bd0c51d12ea4e8d9
buddhi <buddhi.1986@gmail.com>
2020-09-22 00:47:29 +0800

    push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import Pusher from "pusher-js";
 
 function App() {
+    useEffect(() => {
+        console.log('effect')
+
+        const pusher = new Pusher('6b62fa92264becab2041', {
+            cluster: 'ap1'
+        });
+
+        const channel = pusher.subscribe('messages');
+        channel.bind('inserted', function(data) {
+            console.log(JSON.stringify(data));
+        });
+    })
   return (
     <div className="app">
         <div className="app__body">
